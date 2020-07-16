b59af5ab40cef81494ef8cda4cffb14891a69225
petrgali <opelliek@gmal.com>
2020-07-16 21:24:26 +0600

    fixed burstfire mode

--- data.js
+++ data.js
@@ -13,8 +13,9 @@ export const _ = {
     y: 200, // ship start top position
     x: 50, // ship start left position
     shipSpeedY: 1.5, // ship vertical speed
-    shipSpeedX: 1.5,// ship horizontal speed
+    shipSpeedX: 1,// ship horizontal speed
     speedX: 2, // terrain horizontal scrolling speed
+    firingSpeed: 3, // burst fire speed
+    firingRange: 300, // max fire range
     brake: false, // animation hand switch
-    fireRange: 150 // max fire range
 }
--- main.js
+++ main.js
@@ -1,6 +1,81 @@
 
 import { path, sprites, maps, _ } from './data.js'
 
+
+const drawTerrain = () => {
+    document.getElementById('terrain').innerHTML += `<img src=${path}${maps[0]}>`
+    document.getElementById('terrain').classList.add('container')
+}
+const drawShip = () => document.getElementById('mothership').innerHTML += `<img src=${path}${sprites[0]} class='ship'>`
+
+const drawBullet = (axisX, axisY) => document.getElementById('burstfire').innerHTML += `<div class='bullet' 
+style='top:${axisY}px; left:${axisX}px'></div>`;
+
+drawTerrain()
+drawShip()
+
+let terrain = document.getElementById('terrain')
+let ship = document.getElementsByClassName('ship')[0]
+
+//////////////////
+/// CONTROLLER ///
+let burst = document.getElementsByClassName('bullet')
+let shipControlState = {}
+let bullets = []
+
+document.addEventListener('keydown', (event) => {
+    shipControlState[event.key] = true
+})
+document.addEventListener('keyup', (event) => {
+    shipControlState[event.key] = false
+})
+
+let intervalID = setInterval(() => {
+    if (shipControlState[' ']) {
+        bullets.push({ left: _.x + 60, compare: _.x + 60 })
+        drawBullet(_.x + 60, _.y + 10)
+    }
+}, 50)
+
+///////////////////
+///////////////////
+
+
+const shipMove = () => {
+    if (shipControlState['ArrowUp']) _.y -= _.shipSpeedY
+    if (shipControlState['ArrowDown']) _.y += _.shipSpeedY
+    if (shipControlState['ArrowLeft']) _.x -= _.shipSpeedX
+    if (shipControlState['ArrowRight']) _.x += _.shipSpeedX
+    ship.style.top = _.y + 'px'
+    ship.style.left = _.x + 'px'
+}
+const burstFire = () => {
+    for (let idx = 0; idx < bullets.length; idx++) {
+        if ((bullets[idx].left - bullets[idx].compare) >= _.firingRange) {
+            bullets.splice(idx, 1)
+            burst[idx].remove()
+        } else {
+            bullets[idx].left += _.firingSpeed
+            burst[idx].style.left = bullets[idx].left + 'px'
+        }
+    }
+}
+const terrainMove = () => terrain.scrollLeft += _.speedX
+
+
+
+/// VIEW ////
+
+export const render = () => {
+    terrainMove()
+    shipMove()
+    burstFire()
+    if (!_.brake) {
+        window.requestAnimationFrame(render)
+    }
+}
+window.requestAnimationFrame(render)
+
 ////////////////////////
 /////CONTROL MANUAL/////
 let animate = document.getElementsByTagName('button')[0]
@@ -21,72 +96,6 @@ reload.addEventListener('click', function () {
 /////////////
 /////////////
 
-const drawTerrain = () => {
-    document.getElementById('terrain').innerHTML += `<img src=${path}${maps[0]}>`
-    document.getElementById('terrain').classList.add('container')
-}
-const drawShip = () => document.getElementById('mothership').innerHTML += `<img src=${path}${sprites[0]} class='ship'>`
-
-const drawBullet = (x, y) => document.getElementById('burstfire').innerHTML += `<div class='bullet' style='top:${y}px; left:${x}px'></div>`
-
-drawTerrain()
-drawShip()
-
-let terrain = document.getElementById('terrain')
-let ship = document.getElementsByClassName('ship')[0]
-
-//////////////////
-/// CONTROLLER ///
-let shipControlState = {}
-let bullets = []
-
-document.addEventListener('keydown', (event) => {
-    shipControlState[event.key] = true
-})
-document.addEventListener('keyup', (event) => {
-    shipControlState[event.key] = false
-})
-
-let intervalID = setInterval(() => {
-    if (shipControlState[' ']) {
-        bullets.push({ left: _.x + 10, top: _.y })
-        drawBullet(_.x + 60, _.y + 10)
-    }
-}, 50)
-
-// const gunFire = () => {
-//     for (let bullet of bullets) {
-//         bullet
-//     }
-// }
-
-///////////////////
-///////////////////
-
-
-const shipMove = () => {
-    if (shipControlState['ArrowUp']) _.y -= _.shipSpeedY
-    if (shipControlState['ArrowDown']) _.y += _.shipSpeedY
-    if (shipControlState['ArrowLeft']) _.x -= _.shipSpeedX
-    if (shipControlState['ArrowRight']) _.x += _.shipSpeedX
-
-    ship.style.top = _.y + 'px'
-    ship.style.left = _.x + 'px'
-}
-const terrainMove = () => terrain.scrollLeft += _.speedX
-
-
-
-/// VIEW ////
-
-export const render = () => {
-    terrainMove()
-    shipMove()
-    if (!_.brake) {
-        window.requestAnimationFrame(render)
-    }
-}
-window.requestAnimationFrame(render)
 
 ///////////////////
 ///////////////////
